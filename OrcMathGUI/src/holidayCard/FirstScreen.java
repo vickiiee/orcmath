c6e5e38a85e113961bd458de3c6fce61038eaf56
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-12-21 15:01:21 -0500

    V

---
+++ OrcMathGUI/src/holidayCard/FirstScreen.java
@@ -0,0 +1,23 @@
+package holidayCard;
+
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class FirstScreen extends FullFunctionScreen {
+
+	public FirstScreen() {
+		
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		
+	}
+
+}
---
+++ OrcMathGUI/src/holidayCard/SecondScreen.java
@@ -0,0 +1,24 @@
+package holidayCard;
+
+import java.util.List;
+
+import guiTeacher.interfaces.Visible;
+import guiTeacher.userInterfaces.FullFunctionScreen;
+
+public class SecondScreen extends FullFunctionScreen {
+
+	public SecondScreen() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+	}
+
+	@Override
+	public void initAllObjects(List<Visible> viewObjects) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}

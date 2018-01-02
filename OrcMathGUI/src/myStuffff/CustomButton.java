293968d22019e9b0509118165113f011056d7e74
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2018-01-02 15:30:29 -0500

    V

--- OrcMathGUI/src/myStuffff/CustomButton.java
+++ OrcMathGUI/src/myStuffff/CustomButton.java
@@ -1,13 +1,18 @@
 package myStuffff;
 
 import java.awt.Color;
-import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
 
+import guiPlayer.CustomLabel;
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 
 public class CustomButton extends Button implements CustomLabel{
 
+	private String string1;
+	private String string2;
+	private Color colorField;
+	
 	public CustomButton(int x, int y, int w, int h, String text, Action action) {
 		super(x, y, w, h, text, action);
 		// TODO Auto-generated constructor stub
@@ -15,22 +20,23 @@ public class CustomButton extends Button implements CustomLabel{
 
 	@Override
 	public void updateString1(String string) {
-		// TODO Auto-generated method stub
+		string1 = string;
 		
 	}
 
 	@Override
 	public void updateString2(String string) {
-		// TODO Auto-generated method stub
+		string2 = string;
 		
 	}
 
 	@Override
 	public void setIconColor(Color color) {
-		// TODO Auto-generated method stub
-		
+		colorField = color;
 	}
 
+	public void drawButton(Graphics2D g, boolean hover){
+		g.setColor(Color.black); 
 	
-
+	}
 }
